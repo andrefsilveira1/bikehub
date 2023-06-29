43f13a21fb7a5518f73b0545c34bf07e7850bdd1
melosomelo <mateusmelornrn@gmail.com>
2023-06-29 01:51:18 -0300

    add first migration

--- README.md
+++ README.md
@@ -9,5 +9,6 @@ Web project using fastify and vue
 ## Execute backend:
 
 - Create a `.env` file. Take a look at `.env.example`.
+- Run any pending database migrations with `yarn db migrate:latest`
 - If you're using Docker, then just run `docker compose up`.
 - If not, then setup your Postgres database and run the server with `yarn dev`.
---
+++ back/knexfile.js
@@ -0,0 +1,22 @@
+import { config as configDotenv } from "dotenv";
+
+configDotenv();
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+const config = {
+  client: "pg",
+  connection: {
+    host: "localhost",
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+  migrations: {
+    tableName: "knex_migrations",
+    directory: "./database/migrations",
+  },
+};
+
+export default config;
