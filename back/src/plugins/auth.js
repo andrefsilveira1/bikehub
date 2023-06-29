bff4e465967b3d2b966fce78ab9c8729a96910f6
melosomelo <mateusmelornrn@gmail.com>
2023-06-29 17:18:07 -0300

    add authentication hook

--- back/src/plugins/auth.js
+++ back/src/plugins/auth.js
@@ -1,8 +1,18 @@
 import jwt from "jsonwebtoken";
+import APIError from "../errors/APIError.js";
+import { StatusCodes } from "http-status-codes";
 
 export default async function auth(fastify, _, done) {
   fastify.addHook("preHandler", async (req, res) => {
-    console.log("YOOOOOOOOOOOOOOOOOOO");
-    console.log(req.headers);
+    if (!req.headers.authorization)
+      throw new APIError("Authentication required", StatusCodes.BAD_REQUEST);
+    const [, token] = req.headers.authorization?.split(" ");
+    if (!token)
+      throw new APIError("Authentication required", StatusCodes.BAD_REQUEST);
+    try {
+      jwt.verify(token, process.env.JWT_SECRET);
+    } catch {
+      throw new APIError("Authentication failed", StatusCodes.UNAUTHORIZED);
+    }
   });
 }
--- back/src/routes/rentalPoints.js
+++ back/src/routes/rentalPoints.js
@@ -1,4 +1,8 @@
+import fp from "fastify-plugin";
+import auth from "../plugins/auth.js";
+
 export default function rentalPointsRoutes(fastify, _, done) {
+  fastify.register(fp(auth));
   fastify.get("/rentalPoints", async (req, res) => {
     return fastify.services.rentalPoints.get();
   });
