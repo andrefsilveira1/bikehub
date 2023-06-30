a8ade41b459ef1dc5562c7632b876f0050379963
melosomelo <mateusmelornrn@gmail.com>
2023-06-30 19:15:44 -0300

    do some more stuff (preciso sair)

---
+++ front/Bikehub/src/components/RentalPointListing.vue
@@ -0,0 +1,102 @@
+<script setup>
+import { reactive, ref } from "vue";
+import Card from "./cardpoints/index.vue";
+
+let selectedCardId = ref(null);
+let receivedCoordinate = ref("");
+const cards = reactive([
+  {
+    id: 1,
+    title: "IMD",
+    subtitle: "Na frente do centro de convenções",
+    bikenums: "5",
+    lat: -5.8317987316920785,
+    lon: -35.205290890708916,
+  },
+  {
+    id: 2,
+    title: "Centro de Convenções",
+    subtitle: "Próximo ao Senac",
+    bikenums: "9",
+    lat: -5.863775407951932,
+    lon: -35.18072265020563,
+  },
+  {
+    id: 3,
+    title: "Favorito - Neópolis",
+    subtitle: "Do lado da SmartFit",
+    bikenums: "2",
+    lat: -5.878229195496277,
+    lon: -35.20154908599613,
+  },
+  {
+    id: 4,
+    title: "Smart Fit - Roberto Freire",
+    subtitle: "Na rua do Mcdonalds",
+    bikenums: "2",
+    lat: -5.857628344588509,
+    lon: -35.19656159756927,
+  },
+  {
+    id: 5,
+    title: "Mc'Donalds - Abel Cabral",
+    subtitle: "No quinto andar do IMD",
+    bikenums: "2",
+    lat: -5.878389594054686,
+    lon: -35.209369969779885,
+  },
+]);
+
+function selectCard(cardId) {
+  selectedCardId.value = cardId;
+}
+
+function handleCoordinateSelected(coordinate) {
+  receivedCoordinate.value = coordinate;
+}
+</script>
+<template>
+  <div class="grid-container">
+    <div class="left-panel">
+      <div v-for="card in cards" :key="card.id">
+        <Card
+          :title="card.title"
+          :subtitle="card.subtitle"
+          :bikesnum="card.bikenums"
+          :lat="card.lat"
+          :lon="card.lon"
+          @click="selectCard(card.id)"
+          :class="{ selected: card.id === selectedCardId }"
+          @sentCordinate="handleCoordinateSelected"
+        />
+      </div>
+    </div>
+    <div class="right-panel"></div>
+  </div>
+</template>
+
+<style scoped>
+.grid-container {
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  height: 100vh;
+  width: 100%;
+}
+
+.left-panel {
+  background-color: white;
+  padding: 20px;
+  overflow-y: auto;
+  height: 1000px;
+}
+
+.right-panel {
+  overflow-y: hidden;
+}
+p {
+  color: rgb(233, 0, 0);
+}
+.teste {
+  background-color: #eb0000;
+}
+</style>
--- front/Bikehub/src/views/MapView.vue
+++ front/Bikehub/src/views/MapView.vue
@@ -1,9 +1,19 @@
 <script setup>
 import MainLayout from "../components/layouts/MainLayout.vue";
+import RentalPointListing from "../components/RentalPointListing.vue";
 </script>
+
+<style scoped>
+main {
+  display: flex;
+  justify-content: space-between;
+  height: calc(100vh - 120px);
+  width: 100%;
+}
+</style>
 
 <template>
   <MainLayout>
-    <h1>eai</h1>
+    <RentalPointListing />
   </MainLayout>
 </template>
