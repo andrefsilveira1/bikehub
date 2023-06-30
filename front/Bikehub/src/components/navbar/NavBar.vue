526fdf62975c8e375368ce619faf49fc6f5896f5
André Freitas <freitasandre38@gmail.com>
2023-06-29 22:18:56 -0300

    feat: add new views, navbar and vue template

---
+++ front/Bikehub/src/components/navbar/NavBar.vue
@@ -0,0 +1,85 @@
+<script>
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+import { RouterLink } from 'vue-router';
+</script>
+<template>
+    <div class="navbar">
+      <div class="navbar-left">
+        <RouterLink class="nav" to="/">
+            <h1>BikeHub</h1>
+            <i class="bike fas fa-bicycle"></i>
+        </RouterLink>
+      </div>
+      <div class="navbar-right">
+        <ul>
+          <li><RouterLink to="/map">Mapa</RouterLink></li>
+          <li><RouterLink to="/points">Pontos de aluguel</RouterLink></li>
+          <li><RouterLink to="/subscriptions">Suas inscrições</RouterLink></li>
+        </ul>
+      </div>
+    </div>
+  </template>
+
+  
+  <style scoped>
+  .navbar {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+    background-color: #f2f2f2;
+    z-index: 999;
+  }
+  .bike {
+    margin-left: 15px;
+    margin-top: 22px;
+    color: #FF1654;
+    font-size: 50px;
+
+
+  }
+  .nav {
+    display: flex;
+  }
+  .navbar-left {
+    font-size: 30px;
+
+  }
+  
+  .navbar-left h1 {
+    color: #FF1654;
+    margin: 0;
+  }
+  
+  .navbar-right ul {
+    margin: auto;
+    padding: 0;
+    display: flex;
+  }
+  
+  .navbar-right li {
+    color: #FF1654;
+    margin-right: 35px;
+  }
+  
+  .navbar-right a {
+    color: #FF1654;
+  }
+  
+  @media (max-width: 600px) {
+    .navbar {
+      flex-direction: column;
+    }
+  
+    .navbar-left,
+    .navbar-right {
+      margin-bottom: 10px;
+    }
+  }
+  </style>
+  
--- front/Bikehub/src/router/index.js
+++ front/Bikehub/src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import MapView from "../views/MapView.vue"
+import PointsView from "../views/PointsView.vue"
+import SubscriptionsView from "../views/SubscriptionsView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +11,21 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/map',
+      name: 'map',
+      component: MapView
+    },
+    {
+      path: '/points',
+      name: 'points',
+      component: PointsView
+    },
+    {
+      path: '/subscriptions',
+      name: 'subscriptions',
+      component: SubscriptionsView
     },
     {
       path: '/about',
