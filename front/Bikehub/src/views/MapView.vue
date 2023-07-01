f9391094cb35eea5601473c785b66b3eb1338c40
melosomelo <mateusmelornrn@gmail.com>
2023-07-01 14:09:18 -0300

    remove some default vue components

--- front/Bikehub/src/views/MapView.vue
+++ front/Bikehub/src/views/MapView.vue
@@ -7,7 +7,7 @@ import RentalPointListing from "../components/RentalPointListing.vue";
 main {
   display: flex;
   justify-content: space-between;
-  height: calc(100vh - 120px);
+  height: calc(100vh - 120px); /* header height is 120px */
   width: 100%;
 }
 </style>
