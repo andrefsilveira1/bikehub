2b4ebf6e8c8ca5cb55edb8a5b7d624a74dd97229
melosomelo <mateusmelornrn@gmail.com>
2023-07-02 01:21:02 -0300

    start handling auth state

--- front/Bikehub/src/App.vue
+++ front/Bikehub/src/App.vue
@@ -1,7 +1,22 @@
 <script setup>
+import { ref } from "vue";
 import { RouterView } from "vue-router";
+import Loader from "./components/common/Loader.vue";
+
+const loading = ref(true);
+const loggedIn = ref(false);
 </script>
 
 <template>
-  <RouterView />
+  <div
+    v-if="loading"
+    style="
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    "
+  >
+    <Loader :size="15" />
+  </div>
 </template>
