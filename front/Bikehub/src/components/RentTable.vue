03e4a29a02d9b7ac9b0ef243e3fbb6e23a6416e5
melosomelo <mateusmelornrn@gmail.com>
2023-07-02 13:19:05 -0300

    start working on bike returning

--- front/Bikehub/src/components/RentTable.vue
+++ front/Bikehub/src/components/RentTable.vue
@@ -3,24 +3,12 @@ import { onMounted, reactive } from "vue";
 import Button from "./common/Button.vue";
 import api from "../api";
 
-
-const localStorageJwt = localStorage.getItem("bikehubToken");
-const token = localStorageJwt;
-const [headerEncoded, payloadEncoded, signature] = token.split('.');
-const payload = JSON.parse(window.atob(payloadEncoded));
-const userId = payload.user.id;
-console.log("new PAYLOAD:",payload.user.id);
 const bikes = reactive([]);
-
 
 onMounted(async () => {
   try {
-    const data = await api.get(`/user/${userId}/bikes`);
-    if (data.length) {
-        bikes.push(...data);
-    } else {
-        alert("Você ainda não alugou uma bicicleta");
-    }
+    const { data } = await api.get(`/user/bikes`);
+    bikes.push(...data);
   } catch (e) {
     alert(e);
   }
@@ -34,34 +22,15 @@ onMounted(async () => {
         <tr style="border: none">
           <th style="padding: 1.5rem">Identificador</th>
           <th style="padding: 1.5rem">Status</th>
-          <th style="padding: 1.5rem">Horário de Aluguel</th>
-          <th style="padding: 1.5rem">Ponto de aluguel</th>
           <th></th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="(bike, index) in bikes" :key="index">
-          <td>BK{{ bike.id }}</td>
-          <td>{{ bike.available ? "Disponível" : "Alugada" }}</td>
+          <td>BK{{ bike.bikeId }}</td>
+          <td>{{ bike.endTimestamp === null ? "Ativo" : "Devolvida" }}</td>
           <td style="text-align: center">
-            {{
-              bike.lastRentalStartTimestamp
-                ? new Date(bike.lastRentalStartTimestamp).toLocaleDateString(
-                    "pt-BR",
-                    {
-                      day: "2-digit",
-                      month: "2-digit",
-                      year: "numeric",
-                      hour: "2-digit",
-                      minute: "2-digit",
-                      second: "2-digit",
-                    }
-                  )
-                : "-"
-            }}
-          </td>
-          <td style="text-align: center">
-            <Button :disabled="!bike.available">Alugar</Button>
+            <Button :disabled="bike.endTimestamp !== null">Devolver</Button>
           </td>
         </tr>
       </tbody>
--- front/Bikehub/src/views/SubscriptionsView.vue
+++ front/Bikehub/src/views/SubscriptionsView.vue
@@ -1,22 +1,22 @@
 <script setup>
 import MainLayout from "../components/layouts/MainLayout.vue";
-import RentTable from "../components/RentTable.vue"
-
-
+import RentTable from "../components/RentTable.vue";
 </script>
 
 <template>
-        <MainLayout />
-        <RentTable :bikes="bikes"/>
+  <MainLayout>
+    <RentTable :bikes="bikes" />
+  </MainLayout>
 </template>
 
 <style scoped>
-    .slide-enter-active, .slide-leave-active {
-    transition: transform 1.5s;
-    }
+.slide-enter-active,
+.slide-leave-active {
+  transition: transform 1.5s;
+}
 
-    .slide-enter, .slide-leave-to {
-    transform: translateX(100%);
-    }
-
+.slide-enter,
+.slide-leave-to {
+  transform: translateX(100%);
+}
 </style>
